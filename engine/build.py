7e6d584c82e0457f4b09fbd16c25244246c159b6
JRBros2346 <jrbros2346@gmail.com>
2022-10-02 18:53:18 +0530

    Editted
    build.py
    defines.h

--- engine/build.py
+++ engine/build.py
@@ -2,20 +2,20 @@
 import os
 
 #Get a list of all the .cpp files.
-cFilenames=''
+cppFilenames=''
 for (root,dirs,files) in os.walk(os.getcwd()):
     for file in files:
         if file.endswith('.cpp'):
-            cFilenames+=' '+root+file
+            cppFilenames+=' '+root+file
 
-#print("Files:",cfilenames)
+#print("Files:",cppFilenames)
 
 assembly='engine'
 compilerFlags='-g -shared -Wvarargs -Wall -Werror'
 #'-Wall -Werror'
 includeFlags='-Isrc -I%s/Include'%os.environ.get('VULKAN_SDK')
 linkerFlags='-luser32 -lvulkan-1 -L%s/Lib'%os.environ.get('VULKAN_SDK')
-defines=''
+defines='-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS'
 
 print("Building %s..."%assembly)
-os.system('g++ %s %s -o ../bin/%s.dll %s %s %s'%(cFilenames,compilerFlags,assembly,defines,includeFlags,linkerFlags))
+os.system('g++ %s %s -o ../bin/%s.dll %s %s %s'%(cppFilenames,compilerFlags,assembly,defines,includeFlags,linkerFlags))
--- testbed/build.py
+++ testbed/build.py
@@ -2,14 +2,20 @@
 import os
 
 #Get a list of all the .cpp files.
-cfilenames=''
+cppFilenames=''
 for (root,dirs,files) in os.walk(os.getcwd()):
     for file in files:
         if file.endswith('.cpp'):
-            cfilename+=' '+root+file
+            cppFilenames+=' '+root+file
 
-#print("Files:",cfilenames)
+#print("Files:",cppFilenames)
 
 assembly='testbed'
+compilerFlags='-g'
+#'-Wall -Werror'
+includeFlags='-Isrc -I../engine/src/'
+linkerFlags='-L../bin/ -lengine.lib'
+defines='-D_DEBUG -DKIMPORT'
 
 print("Building %s..."%assembly)
+os.system('g++ %s %s -o ../bin/%s.exe %s %s %s'%(cppFilenames,compilerFlags,assembly,defines,includeFlags,linkerFlags))
