9a1c14518fd84518e03ebdd1ee81300f2d595fa0
JRBros2346 <jrbros2346@gmail.com>
2022-10-25 12:18:36 +0530

    Updated build-all.py for better error catching

--- build-all.py
+++ build-all.py
@@ -8,18 +8,18 @@ try:
     os.chdir('engine')
     ERRORLEVEL=os.system('build.py')
     if ERRORLEVEL:
-        sys.exit('Error:%s'%ERRORLEVEL)
+        sys.exit('engine Error(terminal):\033[91m"%s\033[0m'%ERRORLEVEL)
     os.chdir('..')
 except:
-    sys.exit(traceback.format_exc())
+    sys.exit("engine Error(python):\033[91m",traceback.format_exc(),"\033[0m",sep='\n')
 
 try:
     os.chdir('testbed')
     ERRORLEVEL=os.system('build.py')
     if ERRORLEVEL:
-        sys.exit('Error:%s'%ERRORLEVEL)
+        sys.exit('testbed Error(terminal):\033[91m"%s\033[0m'%ERRORLEVEL)
     os.chdir('..')
 except:
-    sys.exit(traceback.format_exc())
+    sys.exit("testbed Error(python):\033[91m",traceback.format_exc(),"\033[0m",sep='\n')
 
 print("All assemblies built successfully.")
