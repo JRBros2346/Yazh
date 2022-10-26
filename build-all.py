c79aa7af376a183c870007e7d4c15edf764bfe99
JRBros2346 <jrbros2346@gmail.com>
2022-10-26 18:05:48 +0530

    Error handling fix

--- build-all.py
+++ build-all.py
@@ -8,18 +8,20 @@ try:
     os.chdir('engine')
     ERRORLEVEL=os.system('python build.py')
     if ERRORLEVEL:
-        sys.exit('engine Error(terminal):\033[91m"%s\033[0m'%ERRORLEVEL)
+        sys.exit('engine Error(terminal): \033[91m%s \033[0m'%ERRORLEVEL)
     os.chdir('..')
 except:
-    sys.exit("engine Error(python):\033[91m",traceback.format_exc(),"\033[0m",sep='\n')
+    exc=sys.exc_info()
+    sys.exit("engine Error(python): \033[91m \ntype: %s \nvalue: %s \ntraceback: %s \033[0m"%(str(exc[0]),str(exc[1]),str(exc[2])))
 
 try:
     os.chdir('testbed')
     ERRORLEVEL=os.system('python build.py')
     if ERRORLEVEL:
-        sys.exit('testbed Error(terminal):\033[91m"%s\033[0m'%ERRORLEVEL)
+        sys.exit('testbed Error(terminal): \033[91m%s \033[0m'%ERRORLEVEL)
     os.chdir('..')
 except:
-    sys.exit("testbed Error(python):\033[91m",traceback.format_exc(),"\033[0m",sep='\n')
+    exc=sys.exc_info()
+    sys.exit("testbed Error(python): \033[91m \ntype: %s \nvalue: %s \ntraceback: %s\033[0m"%(str(exc[0]),str(exc[1]),str(exc[2])))
 
 print("All assemblies built successfully.")
