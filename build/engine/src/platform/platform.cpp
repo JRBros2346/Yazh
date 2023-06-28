6280c7409ea4f62670aae994ea1e6769e63adc3c
JRBros2346 <jrbros2346@gmail.com>
2023-06-28 20:42:12 +0530

    precise sleep

--- build/engine/src/platform/platform.cpp
+++ build/engine/src/platform/platform.cpp
@@ -7,7 +7,35 @@ namespace Yazh {
 	f64 Platform::getAbsoluteTime() {
 		return std::chrono::steady_clock::now().time_since_epoch() / std::chrono::seconds(1);
 	}
-	void sleep(u64 ms) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	
+	void Platform::sleep(f64 ms) {
+		using nano = std::chrono::nanoseconds;
+		using milli = std::chrono::milliseconds;
+		using clock = std::chrono::high_resolution_clock;
+		
+		static f64 estimate = 5e6;
+		static f64 mean = 5e6;
+		static f64 m2 = 0;
+		static u64 count = 1;
+		
+		while (ms > estimate) {
+			auto start = clock::now();
+			std::this_thread::sleep_for(milli(1));
+			auto end = clock::now();
+			
+			nano observed = end - start;
+			ms -= observed.count() / 1e6;
+			
+			++count;
+			f64 delta = observed.count() - mean;
+			mean += delta / count;
+			m2 += delta * (observed.count() - mean);
+			f64 stddev = sqrt(m2 / (count - 1));
+			estimate = mean + stddev;
+		}
+		
+		// spin lock
+		auto start = clock::now();
+		while ((clock::now() - start).count() / 1e6 < ms);
 	}
 }
