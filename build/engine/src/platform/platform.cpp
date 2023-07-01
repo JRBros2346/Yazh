51b8a5846e476b90bdb98940396544619bc994d9
JRBros2346 <jrbros2346@gmail.com>
2023-07-01 21:47:13 +0530

    Working perfectly fine!

--- build/engine/src/platform/platform.cpp
+++ build/engine/src/platform/platform.cpp
@@ -3,6 +3,8 @@
 #include"platform_win32.hpp"
 #include"platform_linux.hpp"
 
+#include<cmath>
+
 namespace Yazh {
 	f64 Platform::getAbsoluteTime() {
 		return std::chrono::steady_clock::now().time_since_epoch() / std::chrono::seconds(1);
